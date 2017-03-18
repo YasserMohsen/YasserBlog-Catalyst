deda3c874e61d09aa247668f5a83d444d16f8198
yasser <yassermohsen101010@gmail.com>
2017-03-19 00:06:22 +0200

    added feature: edit and delete post for users and admin

--- lib/YasserBlog/Controller/Posts.pm
+++ lib/YasserBlog/Controller/Posts.pm
@@ -32,25 +32,74 @@ Catalyst Controller.
 # }
 sub base :Chained('/') :PathPart('posts') :CaptureArgs(0){
     my ( $self, $c ) = @_;
-    
-
+    $c->stash(resultset => $c->model('DB::Post'));
+    # $c->stash->{WRAPPER} = 'wrapper.tt';
 }
 sub add :Chained('base') :PathPart('add') :Args(0){
     my ( $self, $c ) = @_;
     my $content = $c->request->params->{content};
     my $title = $c->request->params->{title};
     my $dt = $c->datetime();
-    my $post = $c->model('DB::Post')->create({
-                userid => $c->user->id,
-                title => $title,
-                content => $content,
-                postdate => $dt,
-                editdate => $dt
-              });
+    if($title && $content){
+        my $post = $c->model('DB::Post')->create({
+                    userid => $c->user->id,
+                    title => $title,
+                    content => $content,
+                    postdate => $dt,
+                    editdate => $dt
+                  });
+    } else {
+        $c->flash->{status_msg} = "Empty title or content!";
+    }
     $c->response->redirect($c->uri_for("/"));
 }
-
-
+sub object :Chained('base') :PathPart('id'): CaptureArgs(1){
+    my ( $self, $c, $id ) = @_;
+    $c->stash(object => $c->stash->{resultset}->find($id));
+    if (!$c->stash->{object}){
+          die;
+          $c->response->redirect($c->uri_for('/'));
+          return;
+    };
+}
+sub delete :Chained('object') :PathPart('delete'): Args(0){
+    my ( $self, $c ) = @_;
+    my $post = $c->stash->{object};
+    if($post->userid->id == $c->user->id || $c->user->role == 'admin'){
+        $c->stash->{object}->delete;
+        $c->flash->{status_msg} = "Post deleted.";
+    }
+    $c->response->redirect($c->uri_for('/'));
+}
+sub edit :Chained('object') :PathPart('edit'): Args(0){
+    my ( $self, $c ) = @_;
+    my $post = $c->stash->{object};
+    if($post->userid->id == $c->user->id  || $c->user->role == 'admin'){
+        $c->stash(edit_post => $c->uri_for($c->controller->action_for("do_edit"),[$post->id]));
+        $c->stash(post => $post, template => 'posts/edit.tt');
+    } else {
+        $c->response->redirect($c->uri_for('/'));
+    }
+}
+sub do_edit :Chained('object') :PathPart('do_edit') :Args(0){
+    my ( $self, $c ) = @_;
+    my $post = $c->stash->{object};
+    my $content = $c->request->params->{content};
+    my $title = $c->request->params->{title};
+    my $dt = $c->datetime();
+    if($title && $content){
+        $c->stash->{object}->update({
+                    title => $title,
+                    content => $content,
+                    editdate => $dt
+                  });
+        $c->flash->{status_msg} = "Post updated.";
+        $c->response->redirect($c->uri_for('/'));
+    } else {
+        $c->flash->{status_msg} = "Empty title or content!";
+        $c->response->redirect($c->uri_for($c->controller->action_for("edit"),[$post->id]));
+    }
+}
 =encoding utf8
 
 =head1 AUTHOR
